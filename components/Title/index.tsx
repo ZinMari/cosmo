3a9eaa8bc8b023756be86bf7d032690ec9f56d93
Mari Zin <mari.v.zinkevich@gmail.com>
2025-05-12 13:16:43 +0300

    feat: upgrade title component

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -3,17 +3,18 @@ import cl from 'classnames';
 import React, { ReactNode } from 'react';
 
 interface TitleProps {
-    size?: 'Medium' | 'Large',
+    size?: 'Medium' | 'Large' | 'Small',
     children: ReactNode,
+    className?: string;
 }
 
 export const Title: React.FC<TitleProps> = ({
     size = 'Medium',
     children,
+    className,
 }) => {
-    console.log(size)
     return (
-        <h2 className={cl(styles.title, styles[`title${size}`])}>
+        <h2 className={cl(styles.title, styles[`title${size}`], className)}>
             {children}
         </h2>
     )
