eff6098bb66ac86f74d1b2bef8b66cf034231163
Mari Zin <mari.v.zinkevich@gmail.com>
2025-05-12 08:41:31 +0300

    feat: create Text component

---
+++ components/Text/index.tsx
@@ -0,0 +1,20 @@
+import cl from 'classnames'
+import styles from './style.module.scss'
+import React from 'react'
+
+interface TextProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export const Text: React.FC<TextProps> = (
+   { children,
+    className,}
+)=>{
+    return (
+        <div className={cl(className, styles.text)}>
+            {children}
+        </div>
+    )
+    
+}
