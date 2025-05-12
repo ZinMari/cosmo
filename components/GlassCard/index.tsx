d9880e19b6103606eefb572beb0bc093b8a20565
Mari Zin <mari.v.zinkevich@gmail.com>
2025-05-12 08:41:00 +0300

    feat: create GlassCard component

---
+++ components/GlassCard/index.tsx
@@ -0,0 +1,18 @@
+import cl from 'classnames';
+
+import styles from './style.module.scss'
+import React from 'react';
+
+interface GlassCardProps {
+    imageUrl: string;
+    type: "color" | 'dark';
+}
+
+export const GlassCard: React.FC<GlassCardProps> = ({
+    imageUrl,
+    type,
+}) => (
+    <div className={cl(styles.classCard, styles[type])}>
+        <img src={imageUrl} alt='Card' className={styles.classCardImage}/>
+    </div>
+)
