0c350f0117c5db8285eaf10ad56eee7072da6bef
Mari Zin <mari.v.zinkevich@gmail.com>
2025-07-25 12:01:09 +0300

    refactor: eslint Text component

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -1,20 +1,12 @@
-import cl from 'classnames'
-import styles from './style.module.scss'
-import React from 'react'
+import cl from "classnames";
+import styles from "./style.module.scss";
+import React from "react";
 
 interface TextProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
-export const Text: React.FC<TextProps> = (
-   { children,
-    className,}
-)=>{
-    return (
-        <div className={cl(className, styles.text)}>
-            {children}
-        </div>
-    )
-    
-}
+export const Text: React.FC<TextProps> = ({ children, className }) => {
+  return <div className={cl(className, styles.text)}>{children}</div>;
+};
