279580e112a94689fadf72298e3dfc4f03707988
Mari Zin <mari.v.zinkevich@gmail.com>
2025-07-25 11:48:14 +0300

    refactor: naming style GlassCard component

--- components/GlassCard/index.tsx
+++ components/GlassCard/index.tsx
@@ -1,26 +1,34 @@
-import cl from 'classnames';
+import cl from "classnames";
 
-import styles from './style.module.scss'
-import React from 'react';
-import Image from 'next/image';
+import styles from "./style.module.scss";
+import React from "react";
+import Image from "next/image";
 
 interface GlassCardProps {
-    imageUrl: string;
-    type: "color" | 'dark';
-    imgNumber: number;
+  imageUrl: string;
+  type: "color" | "dark";
+  imgNumber: number;
 }
 
 export const GlassCard: React.FC<GlassCardProps> = ({
-    imgNumber,
-    imageUrl,
-    type,
+  imgNumber,
+  imageUrl,
+  type,
 }) => {
-    console.log(imgNumber)
-    return (
-        <div className={cl(styles.classCard, styles[type], styles[`classCard__item${imgNumber}`])}>
-        <Image
-            src={imageUrl} alt='Card' className={styles.classCardImage} fill={true}
-        />
+  return (
+    <div
+      className={cl(
+        styles.glassCard,
+        styles[`glassCard_type_${type}`],
+        styles[`glassCard__item${imgNumber}`]
+      )}
+    >
+      <Image
+        src={imageUrl}
+        alt="Card"
+        className={styles.glassCard__image}
+        fill={true}
+      />
     </div>
-    )
-}
+  );
+};
